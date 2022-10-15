601344d5ca2f79502398536c4d17155f861ad782
omason9 <114450076+omason9@users.noreply.github.com>
2022-10-14 22:03:44 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+let allMovies = [];
+
+//Define a movie class with parameters title (string), rating (number) and haveWatched (boolean)
+class Movie {
+    constructor(title, rating, haveWatched) {
+        this.title = title;
+        this.rating = rating;
+        this.haveWatched = haveWatched;
+    }
+}
+
+//add a movie OBJECT to the allMovies array
+let addMovie = (movie) => {
+    allMovies[allMovies.length - 1] = movie;
+}
+
+//iterate through all elements of allMovies array
+//print out to console in a correct format
+//print out the total number of movies in allMovies array
+let printMovies = () => {
+    for (i = 0; i < allMovies.length; i = i + 1) {
+        console.log(allMovies[i]);
+    }
+    console.log(allMovies.length);
+}
+
+
+//print out to console, only the movies that has a rating higher than rating(argument)
+//print out the total number of matches
+let highRatings = (rating) => {
+    for (i = 0; i < allMovies.length; i = i + 1) {
+        if (allMovies[i].rating > rating) {
+            console.log(allMovies[i]);
+        }
+    }
+}
+
+
+//Toggle the 'haveWatched' property of the specified movie 
+let changeWatched = (title) => {
+    for (i = 0; i < allMovies.length; i = i + 1) {
+        if (allMovies[i].title == title) {
+            if (allMovies[i].haveWatched) {
+                allMovies[i].haveWatched = false;
+            } else {
+                allMovies[i].haveWatched = true;
+            }
+        }
+    }
+}
+
+
+
+////////////////////////////////////////////////////////////
+//Test code - DO NOT DELETE OR EDIT
+let x = new Movie("Spiderman", 3, true);
+let y = new Movie("Citizen Kane", 4, false);
+let z = new Movie("Zootopia", 4.5, true);
+
+allMovies.push(x,y,z);
+
+console.log("----------------");
+console.log("running program......");
+console.log("----------------");
+printMovies();
+
+
+let movie1 = new Movie("Parasite", 2, false);
+
+
+console.log("----------------");
+addMovie(movie1);
+console.log("----------------");
+
+
+
+changeWatched("Spiderman");
+console.log("----------------");
+
+printMovies();
+console.log("----------------");
+
+changeWatched("Spiderman");
+console.log("----------------");
+
+printMovies();
+console.log("----------------");
+
+highRatings(3.5);
